4814a2ad4a15777de8c660a89ec692b483974189
seabrea <hgdigm@gmail.com>
2020-03-29 09:54:17 +0800

    完成基本游戏控制

--- SidestepGame/assets/Script/GameController.js
+++ SidestepGame/assets/Script/GameController.js
@@ -12,30 +12,91 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+
+        player: cc.Node,
+        stopTouchLayer: cc.Node,
+        obstaclePrefab: cc.Prefab,
+        scoreLabel: cc.Label
     },
 
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {},
+    onLoad () {
+
+        this.scoreNum = 0,
+        this.timer = 0;
+        this.obstaclePool = new cc.NodePool("checkPoolSize");
+
+        this.scoreLabel.string = "score:" + this.scoreNum;
+
+        this.node.on('touchmove', this.onTouchMoveHandler, this);
+    },
+
+    onDestroy() {
+        this.node.off('touchmove', this.onTouchMoveHandler, this);
+    },
 
     start () {
 
     },
 
-    // update (dt) {},
+    update (dt) {
+
+        this.timer += dt;
+        
+        if (this.timer < 2) {
+            return;
+        }
+
+        this.timer = 0;
+
+        this.createObstacle();
+    },
+
+    onTouchMoveHandler(touch) {
+
+        let vectorDelta = touch.getDelta();
+        this.player.x += vectorDelta.x;
+        this.player.y += vectorDelta.y;
+    },
+
+    checkPoolSize() {
+
+        console.log("当前对象池内对象数量： " + this.obstaclePool.size());
+    },
+
+    createObstacle() {
+
+        var obstacleNode;
+        if (this.obstaclePool.size === 0) {
+            obstacleNode = cc.instantiate(this.obstaclePrefab);
+        }
+        else {
+            obstacleNode = this.obstaclePool.get();
+        }
+
+        this.node.addChild(obstacleNode);
+        obstacleNode.setPosition(this.randPosition());
+        this.runAnimation(obstacleNode);
+    },
+
+    randPosition() {
+        
+        return cc.v2(0, 0);
+    },
+
+    runAnimation(targetNode) {
+        
+        let nodeAction = cc.sequence(cc.moveTo(cc.v2(0,100)), cc.callFunc(()=>{
+
+            targetNode.removeFromParent();
+            this.obstaclePool.put(targetNode);
+
+            this.scoreNum += 1;
+            this.scoreLabel.string = "score:" + this.scoreNum;
+
+            console.log("回收障碍物");
+        }).bind(this));
+        targetNode.runAction(nodeAction);
+    },
 });
